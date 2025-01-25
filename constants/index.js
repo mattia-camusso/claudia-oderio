afeea166726ded9218634225fa72d82ae760ca52
mattia-camusso <mattia.camusso@gmail.com>
2025-01-25 16:18:18 +0100

    riordino

--- constants/index.js
+++ constants/index.js
@@ -6,28 +6,10 @@ export const treatments = [
       "Hai appena partorito? Come sta il tuo bambino? L'osteopatia neonatale è consigliata sin dai primi giorni di vita del bambino per correggere eventuali deformazioni delle ossa del feto causate da fattori esterni durante la gravidanza o da pressioni sul cranio durante il parto. Questi problemi possono causare disturbi come rigurgito, difficoltà nella suzione, coliche, stipsi e irrequietezza. L'osteopatia, tramite una palpazione delicata, aiuta a correggere gli spostamenti impercettibili delle ossa, favorendo una crescita armoniosa del bambino.",
   },
   {
-    id: "terza età",
-    name: "Osteopatia per la Terza Età",
-    description:
-      "Vuoi sentirti più sciolto nei tuoi gesti quotidiani? Con l'avanzare degli anni risulta più difficile svolgere semplici azoni, ma il trattamento osteopatico può aiutare nel ridurre i disagi derivanti dall'invecchiamento, dalla minor mobilità articolare alla sedentarietà. Si suggeriscono anche alcuni esercizi di base per la coordinazione e l'equilibrio, aiutando così la persona a ritrovare più sicurezza nei gesti di ogni giorno.",
-  },
-  {
     id: "infanzia",
     name: "Osteopatia per L'Infanzia",
     description:
       "Tuo figlio cresce in modo sano? Durante la prima visita si ricostruisce insieme ai genitori la storia del bambino per comprendere il momento del parto, le eventuali malattie e traumi, passando successivamente ad esaminare l'asse corporeo e i vari blocchi muscolari. È importante correggere la postura e i movimenti scorretti del bambino, perché possono essere causa di danni ad una colonna vertebrale ancora in crescita",
-  },
-  {
-    id: "gravidanza",
-    name: "Osteopatia in Gravidanza",
-    description:
-      "Sei in dolce attesa? L'osteopatia in gravidanza viene scelta da un numero sempre maggiore di donne. Durante i 9 mesi di gravidanza il corpo della donna viene sottoposto a numerosi e significativi cambiamenti. In queste circostanze la postura si modifica, aumenta il peso e la pressione addominale e possono sorgere alcuni disturbi come mal di schiena e dolori alle gambe. L'osteopatia può migliorare la qualità della vita della donna permettendole di vivere in modo naturale e sereno i cambiamenti che si verificano nell'arco di questi 9 mesi.",
-  },
-  {
-    id: "sport",
-    name: "Osteopatia per lo Sport",
-    description:
-      "Sei uno sportivo? L’osteopatia supporta gli atleti ottimizzando le performance, prevenendo infortuni e favorendo il recupero post-allenamento o gara. Con trattamenti mirati, migliora la funzionalità muscolare e articolare, offrendo un approccio personalizzato per professionisti e amatori. L’obiettivo è mantenere il corpo in equilibrio, ridurre il dolore da infortuni e garantire la piena mobilità dell’apparato muscolo-scheletrico.",
   },
   {
     id: "adolescenza",
@@ -35,6 +17,26 @@ export const treatments = [
     description:
       "Hai notato posture scorrette o dolori in tuo figlio durante la crescita? L’osteopatia interviene su squilibri causati da cadute, sbalzi di crescita e muscolatura debole, spesso all’origine di dolori cervicali, lombari o spalle curve. L’osteopata corregge movimenti, migliora la respirazione e offre consigli su zaino, postura e uso dei dispositivi. Non solo: tiene conto del legame tra cambiamenti fisici, ormonali ed emozioni, aiutando i ragazzi a ritrovare equilibrio tra corpo in evoluzione e mondo esterno.",
   },
+  {
+    id: "gravidanza",
+    name: "Osteopatia in Gravidanza",
+    description:
+      "Sei in dolce attesa? L'osteopatia in gravidanza viene scelta da un numero sempre maggiore di donne. Durante i 9 mesi di gravidanza il corpo della donna viene sottoposto a numerosi e significativi cambiamenti. In queste circostanze la postura si modifica, aumenta il peso e la pressione addominale e possono sorgere alcuni disturbi come mal di schiena e dolori alle gambe. L'osteopatia può migliorare la qualità della vita della donna permettendole di vivere in modo naturale e sereno i cambiamenti che si verificano nell'arco di questi 9 mesi.",
+  },
+
+  {
+    id: "sport",
+    name: "Osteopatia per lo Sport",
+    description:
+      "Sei uno sportivo? L’osteopatia supporta gli atleti ottimizzando le performance, prevenendo infortuni e favorendo il recupero post-allenamento o gara. Con trattamenti mirati, migliora la funzionalità muscolare e articolare, offrendo un approccio personalizzato per professionisti e amatori. L’obiettivo è mantenere il corpo in equilibrio, ridurre il dolore da infortuni e garantire la piena mobilità dell’apparato muscolo-scheletrico.",
+  },
+  {
+    id: "terza età",
+    name: "Osteopatia per la Terza Età",
+    description:
+      "Vuoi sentirti più sciolto nei tuoi gesti quotidiani? Con l'avanzare degli anni risulta più difficile svolgere semplici azoni, ma il trattamento osteopatico può aiutare nel ridurre i disagi derivanti dall'invecchiamento, dalla minor mobilità articolare alla sedentarietà. Si suggeriscono anche alcuni esercizi di base per la coordinazione e l'equilibrio, aiutando così la persona a ritrovare più sicurezza nei gesti di ogni giorno.",
+  },
+
   /*   {
     id: "sacrale",
     name: "Tecniche cranio-sacrali",
